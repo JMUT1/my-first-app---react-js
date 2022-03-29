51e6e18c9ef4254b2025f3ca67e950a37c923b3b
Chema Torres <chemaaa94@gmail.com>
2022-03-28 19:52:37 -0600

    First component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 
 
 function App() {
-  return <div> QUE TAL </div>;
+  return <div>
+    <h1>MY TODOS</h1>
+    <div>
+      <h2>TITLE</h2>
+      <div>
+      <button>Delete</button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
