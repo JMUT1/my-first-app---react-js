a22d16fecd30849ca6830aa15ecb6e8e5d36f78c
Chema Torres <chemaaa94@gmail.com>
2022-03-28 19:59:39 -0600

    components-first-changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 function App() {
   return <div>
     <h1>MY TODOS</h1>
-    <div>
+    <div className="card">
       <h2>TITLE</h2>
-      <div>
-      <button>Delete</button>
+      <div className="actions">
+      <button className="btn">Delete</button>
       </div>
     </div>
   </div>;
